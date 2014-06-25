dd2ad0b0fef29567a398bd31741993fbf2c78a9f
jpneirac <jupaneira@gmail.com>
2014-06-24 21:35:00 -0500

    Revisando JP

--- Competencia/agents/agent-examples/src/unalcol/agents/examples/reversi/AgenteReversiPerla.java
+++ Competencia/agents/agent-examples/src/unalcol/agents/examples/reversi/AgenteReversiPerla.java
@@ -11,36 +11,34 @@ import unalcol.agents.Percept;
  *
  * @author JuanFelipe
  */
-public class AgenteReversiPerla implements AgentProgram{
+public class AgenteReversiPerla implements AgentProgram {
+
     protected String color;
     protected int tam;
     protected int fvez = 0;
     protected Tablero tablero;
     Map<String, Integer> valor = new HashMap<>();
-    
-    public AgenteReversiPerla( String color ) {
+
+    public AgenteReversiPerla(String color) {
         this.color = color;
         valor.put("space", 0);
         valor.put("white", 1);
         valor.put("black", -1);
     }
 
-    
     @Override
-    public Action compute( Percept p ) {
-        System.out.println("TURNO: "+p.getAttribute(Reversi.TURN));
-        if( fvez==0 ){
+    public Action compute(Percept p) {
+
+        if (fvez == 0) {
             fvez = 1;
-            tam = Integer.parseInt( p.getAttribute(Reversi.SIZE).toString() );
-            System.out.println("TAM:"+tam);
+            tam = Integer.parseInt(p.getAttribute(Reversi.SIZE).toString());
             tablero = new Tablero(tam);
-            leerTablero( p );
         }
-        
-        if( p.getAttribute(Reversi.TURN).equals(color) ) {
-            leerTablero( p );
-            tablero.identificarFichas( );
-            ArrayList<Integer> posibleMov = tablero.movimientos( color );
+
+        if (p.getAttribute(Reversi.TURN).equals(color)) {
+            leerTablero(p);
+            tablero.identificarFichas();
+            ArrayList<Integer> posibleMov = tablero.movimientos(color);
             int idx = 0;
 //            System.out.println("POSIBLES MOV");
 //            for (Integer num : posibleMov) {
@@ -52,33 +50,29 @@ public class AgenteReversiPerla implements AgentProgram{
 //            }
 //            System.out.println("FIN POSIBLES MOV");
             System.out.println("Movimiento:" + color);
-            System.out.println("->X:"+posibleMov.get(0)+" Y:"+posibleMov.get(1));
+            System.out.println("->X:" + posibleMov.get(0) + " Y:" + posibleMov.get(1));
             System.out.println(tablero.print());
-            return new Action( posibleMov.get(0)+":"+posibleMov.get(1)+":"+color );
+            return new Action(posibleMov.get(0) + ":" + posibleMov.get(1) + ":" + color);
         }
-        
-        System.out.println("Stealing turn "+color);
-        return new Action( Reversi.PASS );
-    }
 
-    @Override
-    public void init() {
-        
+        System.out.println("Stealing turn " + color);
+        return new Action(Reversi.PASS);
     }
 
     private void leerTablero(Percept p) {
         String pos, casilla;
-        
-        //System.out.println("Tablero");
-        for( int i=0; i< tam; i++){
-            for( int j=0; j< tam; j++){
-                pos = i+":"+j;
+        for (int i = 0; i < tam; i++) {
+            for (int j = 0; j < tam; j++) {
+                pos = i + ":" + j;
                 casilla = p.getAttribute(pos).toString();
                 tablero.tablero[i][j] = valor.get(casilla);
-                //System.out.print(" "+tablero.tablero[i][j]);
             }
-            //System.out.println("");
         }
     }
-    
+
+    @Override
+    public void init() {
+
+    }
+
 }
--- Competencia/agents/agent-examples/src/unalcol/agents/examples/reversi/Tablero.java
+++ Competencia/agents/agent-examples/src/unalcol/agents/examples/reversi/Tablero.java
@@ -7,31 +7,32 @@ import java.util.ArrayList;
  * @author JuanFelipe
  */
 class Tablero {
-    static final  int movX[] = {-1,-1,-1, 0, 0, 1, 1, 1};
-    static final  int movY[] = {-1, 0, 1,-1, 1,-1, 0, 1};
+
+    static final int movX[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+    static final int movY[] = {-1, 0, 1, -1, 1, -1, 0, 1};
     int tablero[][];
     int tam;
     ArrayList<Integer> posicionBlancas;
     ArrayList<Integer> posicionNegras;
-    
-    public Tablero( int tam ) {
-        this( new int[tam][tam], tam);
+
+    public Tablero(int tam) {
+        this(new int[tam][tam], tam);
     }
-    
+
     public Tablero(int[][] tablero, int tam) {
         this.tablero = tablero;
         this.tam = tam;
         posicionBlancas = new ArrayList<>();
         posicionNegras = new ArrayList<>();
     }
-    
+
     public void identificarFichas() {
         for (int i = 0; i < tam; i++) {
             for (int j = 0; j < tam; j++) {
-                if( tablero[i][j]==1 ){
+                if (tablero[i][j] == 1) {
                     posicionBlancas.add(i);
                     posicionBlancas.add(j);
-                }else if( tablero[i][j]==-1 ){
+                } else if (tablero[i][j] == -1) {
                     posicionNegras.add(i);
                     posicionNegras.add(j);
                 }
@@ -40,45 +41,48 @@ class Tablero {
     }
 
     /**
-     * 
+     *
      * @param colorS
-     * @return 
+     * @return
      */
     ArrayList<Integer> movimientos(String colorS) {
-        int color = colorS.equals(Reversi.WHITE)?1:-1;
-        ArrayList<Integer>posiciones = (color==1)?posicionBlancas:posicionNegras;
-        ArrayList<Integer>movimientos = new ArrayList<>();
-        
+        int color = colorS.equals(Reversi.WHITE) ? 1 : -1;
+        ArrayList<Integer> posiciones = (color == 1) ? posicionBlancas : posicionNegras;
+        ArrayList<Integer> movimientos = new ArrayList<>();
+
         int x, y;
         int posMov[];
-        for (int i=0; i<posiciones.size(); i+=2) {
+        for (int i = 0; i < posiciones.size(); i += 2) {
             x = posiciones.get(i);
-            y = posiciones.get(i+1);
-            for( int idx=0; idx<8; idx++){
-                System.out.println("Manda:"+x+" "+y+" "+colorS);
-                 posMov = mov( x+movX[idx], y+movY[idx], movX[idx], movY[idx], color ) ;
-                 if( posMov[0]==1 ){
-                     movimientos.add(posMov[1]);
-                     movimientos.add(posMov[2]);
-                 }
+            y = posiciones.get(i + 1);
+            for (int idx = 0; idx < 8; idx++) {
+                System.out.println("Manda:" + x + " " + y + " " + colorS);
+                posMov = mov(x + movX[idx], y + movY[idx], movX[idx], movY[idx], color);
+                if (posMov[0] == 1) {
+                    movimientos.add(posMov[1]);
+                    movimientos.add(posMov[2]);
+                }
             }
-            
+
         }
-        
-        
+
         return movimientos;
     }
 
     /**
-     * Devuelve 3 valores, 
-     * <p/>el 1 si es valido ese movimiento
-     * <p/>el 2 la posicion x valida
-     * <p/>el 3 la posicion y valida
+     * Devuelve 3 valores,
+     * <p/>
+     * el 1 si es valido ese movimiento
+     * <p/>
+     * el 2 la posicion x valida
+     * <p/>
+     * el 3 la posicion y valida
+     *
      * @param x
      * @param y
      * @param i
      * @param i0
-     * @return 
+     * @return
      */
     private int[] mov(int x, int y, int movx, int movy, int color) {
         int[] ret = {0, -1, -1};
@@ -86,17 +90,17 @@ class Tablero {
         int xo, yo;
         xo = x;
         yo = y;
-        while( isValid(x, y) && tablero[x][y] == -color ){
+        while (isValid(x, y) && tablero[x][y] == -color) {
             x += movx;
             y += movy;
             z = 1;
         }
-        if( x==1 && y==3 && color==1){
-            System.out.println("ACA TABLERO"+x+" "+y+" "+xo+" "+yo);
+        if (x == 1 && y == 3 && color == 1) {
+            System.out.println("ACA TABLERO" + x + " " + y + " " + xo + " " + yo);
             System.out.println(print());
-            System.out.println("FIN TABLERO"+x+" "+y+" "+xo+" "+yo);
+            System.out.println("FIN TABLERO" + x + " " + y + " " + xo + " " + yo);
         }
-        if( isValid(x, y) && tablero[x][y]==0 && z==1){
+        if (isValid(x, y) && tablero[x][y] == 0 && z == 1) {
             ret[0] = 1;
             ret[1] = x;
             ret[2] = y;
@@ -112,15 +116,15 @@ class Tablero {
     }
 
     private boolean isValid(int x, int y) {
-        return x>=0 && x<tam && y>=0 && y<tam;
+        return x >= 0 && x < tam && y >= 0 && y < tam;
     }
-    
-    public String print(){
+
+    public String print() {
         StringBuilder sb = new StringBuilder();
         sb.append("\n\n***\n");
-        for( int i=0; i<tam; i++){
-            for(int j=0; j<tam; j++){
-                sb.append("|"+tablero[i][j]+"|");
+        for (int i = 0; i < tam; i++) {
+            for (int j = 0; j < tam; j++) {
+                sb.append("|" + tablero[i][j] + "|");
             }
             sb.append("\n");
         }
